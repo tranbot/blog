05b499997fe102bd406b22642f2eb318bb086048
John Tran <johntran@Johns-MacBook-Pro.local>
2019-12-17 09:05:15 -0800

    building reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import jsonPlaceHolder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
   // we don't always use 'getState'
   const response = await jsonPlaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
-  temp: () => 0
+  posts: postsReducer,
+  users: usersReducer
 });
