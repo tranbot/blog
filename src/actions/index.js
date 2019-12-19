d1080f537bd50964360e4dfe4a3663fb134cc04d
John Tran <johntran@Johns-MacBook-Pro.local>
2019-12-19 00:00:16 -0800

    completed blog app

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,18 @@ import _ from "lodash";
 
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts) //refactor using chain
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); // .value() required to end the chain
+};
+
 export const fetchPosts = () => async dispatch => {
   // we don't always use 'getState'
   const response = await jsonPlaceholder.get("/posts");
@@ -9,13 +21,22 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   });
-});
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data
+//   });
+// });
